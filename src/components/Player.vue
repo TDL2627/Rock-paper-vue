bc12e8b006ac18303263de7aebe656bb542e0efe
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-09 13:34:42 +0200

    added bot functionality

---
+++ src/components/Player.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+  <button v-on:click="randomChoice()"> PLAYGAME </button>
+  </div>
+</template>
+
+<script>
+const choices = ["rock", "paper", "scissors"];
+export default {
+  data() {
+    return {
+      choice: ""
+    };
+  },
+  methods: {
+    randomChoice() {
+	const randomChoiceIndex = Math.floor(Math.random() * choices.length);
+	this.choice = choices[randomChoiceIndex];
+	this.$emit('getChoice', this.choice);
+    },
+  }
+};
+</script>
+
+<style scoped>
+button {
+  margin: 0 auto;
+  margin-bottom: 20px;
+  padding: 15px 32px;
+  background-color: #4CAF50;
+  display: inline-block;
+  font-size: 16px;
+  color: #fff;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #54e459;
+}
+</style>
