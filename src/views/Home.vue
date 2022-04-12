95eaa4c44db7b1d54603fe275361cfc9b2930daf
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-12 14:04:18 +0200

    removed annoying css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,34 +5,23 @@
       </div>
       <!-- ScoreBoard -->
       <div class="scoreboard">
-    <div class="scorecontainer">
+  
       <h2 class="score_text">ScoreBoard</h2>
-      <div class="float-child">
-        <div class="player">
-          <h3>You</h3>
-          <h3 class="score">{{player_score}}</h3>
-        </div>
+        <div class="score">
+          <h3>YOU : {{player_score}}</h3>
+          <h3>OPP : {{opponent_score}}</h3>
       </div>
-      <div class="float-child">
-        <div class="oponent">
-          <h3>Opponent</h3>
-          <h3 class="score">{{opponent_score}}</h3>
-        </div>
-      </div>
-    </div>
   </div>
-  <div class="instructions">
-    <h3 >Press on your chosen image and then press PLAYGAME!</h3>
-  </div>
+    
+
       <!-- Game -->
     <div class="float-child1">
       <Player v-on:getChoice="updatePlayerChoice($event)" v-bind:choice="player_choice" />
-      <h2 style="color:white">You chose: {{player_choice}}</h2>
+      <h2 style="color:black;">You chose: {{player_choice}}</h2>
     </div>
     <div class="float-child2">
       <Opponent @click="play" v-on:getChoice="updateOpponentScore($event)" v-bind:choice="opponent_choice"/>
-      
-      <img type="button" v-if="!opponent_choice" src="../assets/quest.png" /> 
+      <img type="button" class="quest" v-if="!opponent_choice" src="../assets/quest.png" /> 
       <img class="opponent_img" type="button" v-if="!!opponent_choice" :src="require('../assets/'+ImgSrc())" />
       
       <h2 style="color:white">Opponents choice: {{opponent_choice}}</h2>
@@ -107,27 +96,10 @@ export default {
 }
 </script>
 <style scoped>
-*, .body {
-    background-color: #292c34;
-    overflow: hidden;
+.quest{
+height: 50px;
 }
-* {
-    margin: 0;
-    padding: 0;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    font-family: Asap, sans-serif;
-    font-weight: 700;
-}
-.instructions {
-  padding: 10px;
-  margin-left: 200px;
-  width: 70%;
-}
-.instructions h3 {
-  text-align: center;
-  border: 2px solid gold;
-}
+
 .header-container {
     background: #fff;
     padding: 25px;
@@ -139,21 +111,8 @@ export default {
   text-align: center;
   font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
 }
-.scoreboard {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-}
-.scorecontainer {
-  margin-top: 20px;
-  padding: 20px;
-  width: 25%;
-  height: 200px;
-  background-color:aliceblue;
-  border: 3px solid rgb(68, 59, 151);
-  border-radius: 10px;
-}
+
+
 .score_text {
   background-color: aliceblue;
   text-align: center;
@@ -168,13 +127,7 @@ export default {
   padding: 20px;
   
 }
-.float-child1 {
-  background-color:#292c34;
-  width: 50%;
-  float: left;
-  padding: 20px;
-  margin-left:200px;
-}
+
 .float-child2 {
   margin-top:200px;
 }
@@ -197,11 +150,12 @@ h3 {
   border-radius: 10px;
 }
 .winner {
-  width: 320px;
+
+  z-index:2922134;
   padding: 10px;
   border: 5px solid gray;
   margin: 0;
   background-color: coral;
-  color:#fff;
+  color:black;
 }
 </style>
